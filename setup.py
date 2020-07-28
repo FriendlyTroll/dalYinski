395c204d1beccf16c6212ebb78e92180a81dbff5
Ante Maretić <antisa@pm.me>
2020-07-28 16:59:51 +0200

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'dalyinski',
+    version = '0.1',
+    description = 'Server for dalYinski Android app',
+    license='GPL v3',
+    author = 'antisa',
+    packages = find_packages(),
+    entry_points={
+        'console_scripts': [
+    'dalyinski-server = server.server:main']}
+)
