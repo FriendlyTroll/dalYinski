c004a10225245a50f8afef40c06cb10a0a54cef0
antisa <antisa@pm.me>
2020-09-18 11:22:22 +0200

    Add system tray icon,version info and notifications for gui

--- dalyinski/server/gui.py
+++ dalyinski/server/gui.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+__version__ = '0.4'
+
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk, Gio, Notify
 
 from dalyinski.server.server import ServerConn
 
@@ -26,17 +29,19 @@ class Win (Gtk.Window):
         # add the startserver to the vbox
         vbox.pack_start(startserver, expand=True, fill=True, padding=0)
         vbox.pack_start(quit_btn, expand=True, fill=True, padding=0)
-        # create a label to say *something* and add to the vbox
+        # create a label and add to the vbox
         label=Gtk.Label("Server GUI for dalYinski")
+        version_label=Gtk.Label(f'GUI version: {__version__}')
         vbox.pack_start(label, expand=True, fill=True, padding=0)
+        vbox.pack_start(version_label, expand=True, fill=True, padding=0)
         #add the vbox to the Window
         self.add(vbox)
         #show all of the stuff
         self.show_all()
         #make a status icon
-        self.statusicon = Gtk.StatusIcon.new_from_stock(Gtk.STOCK_GOTO_TOP)
+        self.statusicon = Gtk.StatusIcon.new_from_file('/usr/share/pixmaps/dalyinski-server.png')
         self.statusicon.connect('activate', self.status_clicked )
-        self.statusicon.set_tooltip_text("the window is visible")
+        self.statusicon.set_tooltip_text("The server is NOT running.")
         self.server = ServerConn()
 
         #start the Gtk main loop
@@ -44,6 +49,13 @@ class Win (Gtk.Window):
     
     def run_server(self, button):
         self.server.run()
+        # set tooltip
+        self.statusicon.set_tooltip_text("The server is running.")
+
+        # show a notification as well
+        Notify.init('dalYinski server')
+        noticon = Notify.Notification.new('dalYinski server', 'Server started', '/usr/share/pixmaps/dalyinski-server.png')
+        noticon.show()
 
     def quit(self,button):
         #quit the Gtk main loop
@@ -59,7 +71,7 @@ class Win (Gtk.Window):
     def status_clicked(self,status):
         #unhide the window
         self.show_all()
-        self.statusicon.set_tooltip_text("the window is visible")
+        # self.statusicon.set_tooltip_text("the window is visible")
         
 if __name__=="__main__":
     win = Win()
